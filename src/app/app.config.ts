6f7fa2ad316b652a9275d3833012c9e026e3406d
Bastian Krones <kronesbastian@gmail.com>
2025-08-01 23:17:39 +0200

    start

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "gamer-search", appId: "1:692620794947:web:79d17bc46bed9034e588f6", storageBucket: "gamer-search.firebasestorage.app", apiKey: "AIzaSyCaxy4ZxYOiSfhkTA5aJKehQBOjXNqGmEc", authDomain: "gamer-search.firebaseapp.com", messagingSenderId: "692620794947" })), provideFirestore(() => getFirestore())
   ]
 };
